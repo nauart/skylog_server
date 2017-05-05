f263fc97c67efba9ccfcd5518b44ef87c21a7ac1
Artem Nosach <ANosach@luxoft.com>
2017-05-05 15:46:03 +0300

    temp

---
+++ src/main.cc
@@ -0,0 +1,107 @@
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class tcp_connection
+  : public boost::enable_shared_from_this<tcp_connection>
+{
+public:
+  typedef boost::shared_ptr<tcp_connection> pointer;
+
+  static pointer create(boost::asio::io_service& io_service)
+  {
+    return pointer(new tcp_connection(io_service));
+  }
+
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
+
+  void start() {
+      message_.clear();
+      message_.resize(1000);
+      boost::asio::async_read(socket_, boost::asio::buffer(&message_[0], message_.size()),
+              boost::bind(&tcp_connection::handle_read, shared_from_this(),
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+  }
+
+  void handle_read(const boost::system::error_code& error,
+        size_t bt)
+  {
+        std::cout << "Read " << bt << "bytes!\n";
+        std::cout << message_ << "\n";
+     if (!error) {
+         start();
+     }
+  }
+
+private:
+  tcp_connection(boost::asio::io_service& io_service)
+    : socket_(io_service)
+  {
+  }
+
+  tcp::socket socket_;
+  std::string message_;
+};
+
+class tcp_server
+{
+public:
+  tcp_server(boost::asio::io_service& io_service)
+    : acceptor_(io_service, tcp::endpoint(tcp::v4(), 6666))
+  {
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    tcp_connection::pointer new_connection =
+      tcp_connection::create(acceptor_.get_io_service());
+
+    acceptor_.async_accept(new_connection->socket(),
+        boost::bind(&tcp_server::handle_accept, this, new_connection,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(tcp_connection::pointer new_connection,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      std::cout << "Accepted!\n";
+      new_connection->start();
+      start_accept();
+    }
+  }
+
+  tcp::acceptor acceptor_;
+};
+
+int main()
+{
+  boost::asio::io_service io_service;
+  tcp_server server(io_service);
+  io_service.run();
+
+  return 0;
+}
